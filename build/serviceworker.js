6e68fff086b2f9cd1975df5b7b52b89e8af5ef01
Manuel Lieb <manuel@pixelschubser.ch>
2018-09-02 17:03:51 +0200

    [TASK] added pwa stuff

---
+++ build/serviceworker.js
@@ -0,0 +1,52 @@
+var cacheName = 'scrumpoker:0001';
+var cacheFiles = [
+    '/',
+    'index.html',
+    'css/main.css',
+    'css/components/poker.css',
+    'js/poker.js'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(function(cache) {
+                //console.log('Opened cache');
+                return cache.addAll(cacheFiles);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // Grab the asset from SW cache.
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }).catch(function() {
+            // Can't access the network return an offline page from the cache
+            return caches.match('offline.html');
+        })
+    );
+});
+
+// Empty out any caches that don’t match the ones listed.
+self.addEventListener('activate', function(event) {
+
+    var cacheWhitelist = ['scrumpoker:0001'];
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
